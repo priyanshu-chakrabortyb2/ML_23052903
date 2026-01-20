b03e37171a00baef84655e7f7b1ad94a566abfd2
PRIYANSHU <priyanshuchakraborty179@gmail.com>
2026-01-21 02:25:12 +0530

    Decision tree

---
+++ Models/Decision tree/model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("iris.csv")
+
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
